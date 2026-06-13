05919ace6db75890b1e11b9823ff9fcbb43ba7f7
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-13 21:49:49 +0200

    Update rollup.config.js
    Definice rollup publishing systému

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,10 @@
-# rollup config publishing system
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/pocasimeteo-card.js",
+  output: {
+    file: "dist/pocasimeteo-card.js",
+    format: "es"
+  },
+  plugins: [terser()]
+};
