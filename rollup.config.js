cbc2b436ac897eed964ecde04dc1702aaf39b124
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-14 03:23:08 +0200

    Update rollup.config.js
    Doplněný import charts.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
-import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/pocasimeteo-card.js",
   output: {
     file: "dist/pocasimeteo-card.js",
-    format: "es"
+    format: "es",
   },
-  plugins: [terser()]
+  plugins: [
+    resolve(),
+    commonjs(),
+  ]
 };
