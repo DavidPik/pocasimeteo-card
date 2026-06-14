196eb4743bf8efbc231a700a8872094d054a1640
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-14 10:55:20 +0200

    Update rollup.config.js
    Přechod na nový způsob bundlování knihoven chart.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/pocasimeteo-card.js",
   output: {
     file: "dist/pocasimeteo-card.js",
-    format: "esm",
+    format: "esm"
   },
   plugins: [
     resolve(),
+    commonjs()
   ]
 };
